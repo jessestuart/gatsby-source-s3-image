e371043649c3ce51a9f7bd1be389244d415767c4
Jesse Stuart <hi@jessestuart.com>
2019-04-25 11:17:21 -0400

    fix(babel): Revert to babel 7.3.x and core-js@2.
    Signed-off-by: Jesse Stuart <hi@jessestuart.com>

--- babel.config.js
+++ babel.config.js
@@ -3,17 +3,6 @@ module.exports = {
   comments: false,
   sourceRoot: 'src/',
   ignore: ['./src/__tests__/*', './src/types/*'],
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        // For more info on babel + core-js v3,
-        // @see https://babeljs.io/blog/2019/03/19/7.4.0
-        useBuiltIns: 'usage',
-        corejs: 3,
-      },
-    ],
-    '@babel/preset-typescript',
-    'minify',
-  ],
+  presets: ['@babel/preset-env', '@babel/preset-typescript', 'minify'],
+  plugins: ['@babel/plugin-transform-runtime'],
 }
