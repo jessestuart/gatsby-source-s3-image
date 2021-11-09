91fc655a56d389cb4f4fd1c2d8ca8bca84e5648d
Marc Easen <marc@easen.co.uk>
2021-11-09 17:20:13 +0000

    build: can be added as a git repo dependancy

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
-import ExifData from './types/ExifData';
-import S3ImageAssetNode from './types/S3ImageAssetNode';
-export { ExifData, S3ImageAssetNode };
+import ExifData from './types/ExifData'
+import S3ImageAssetNode from './types/S3ImageAssetNode'
+export { ExifData, S3ImageAssetNode }
